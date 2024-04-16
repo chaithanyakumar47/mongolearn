19d835e51cb533d453cfab9a0353d7bd458cd01e
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2024-04-17 00:08:32 +0530

    Basic Add to cart functionality

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('661e80dc0d53d6ef150b522e')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id)
       next();
     })
     .catch(err => console.log(err));
