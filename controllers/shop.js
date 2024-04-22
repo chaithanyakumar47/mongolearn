b94418f115a1c232cdb736b3dcb325fbb9f6bf3c
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2024-04-22 23:49:43 +0530

    Everything else finished

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,7 +83,7 @@ exports.postOrder = (req, res, next) => {
   .populate('cart.items.productId')
   .then(user => {
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: i.productId }
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
       })
       const order = new Order({
         user: {
@@ -96,20 +96,23 @@ exports.postOrder = (req, res, next) => {
     } 
   )
     .then(result => {
+      return req.user.clearCart()
+      
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
+  Order.find({ 'user.userId': req.user._id })
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
