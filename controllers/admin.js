7dfb2adfac15737887ba48d445ccca311d12454b
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2024-04-22 18:31:56 +0530

    Mongoose works with fetching and updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id)
+  const product = new Product({
+    title: title, 
+    price: price, 
+    description: description, 
+    imageUrl: imageUrl
+  })
   product
   .save()  
   .then(result => {
@@ -54,14 +59,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(
-    updatedTitle,
-    updatedPrice, 
-    updatedDesc, 
-    updatedImageUrl, 
-    prodId
-  )
-  product.save()  
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl
+    return product.save()  
+  })
   .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -70,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-     Product.fetchAll()
+     Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
